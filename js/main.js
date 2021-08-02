fa228377c93dfe9c18b6544ddd68ea03b2088ef9
Jose T. Genao <blacksaga26@gmail.com>
2021-08-02 16:55:49 -0400

    Actualizacion main.js (solo falta optimizarlo y limpiarlo)

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
-const btnAgregar = document.querySelector('form button');
+const btnAgregar = document.querySelector('form #btnAgregar');
 const btnBorrar = document.querySelector('#btnBorrar');
 const btnCalcular = document.querySelector('#btnCalcular');
 const form = document.querySelector("[name='agregarAsignatura']");
 const nombreAsignatura = form.asignatura;
-const calificacionInput = form.calificacion;
+let calificacionInput = form.calificacionNumero; // !por defecto
 const creditosInput = form.creditos;
 const tabla = document.querySelector("#tabla tbody");
 let datos = {
@@ -14,25 +14,74 @@ let datos = {
     promedio: 0
 }
 
+// !Opcion 1 Las letras sean identificadas como numeros
+// Poner input calificacion que acepte letras y hacer una validacion de que los nummeros sean
+// de 0 a 100; y que las letras sea identificadas (por medio de un if) como numeros (4, 3, 2, 1, 0)
+// - (A, B, C, D, F) respectivamente
+
+document.querySelector('#calificacionLetras').style.display = 'none';
+document.querySelector('#calificacionNumero').style.display = 'inline';
+const calificacionTipo = form.querySelectorAll('[name="calificacionTipo"]');
+var tipo
+
+function seleccionarTipoCalificacion(){
+    for(let i = 0; i < calificacionTipo.length; i++){
+        if(calificacionTipo[i].checked){
+            tipo = calificacionTipo[i].value
+        }
+    }
+    if(tipo === 'base100'){
+        document.querySelector('#calificacionLetras').style.display = 'none';
+        document.querySelector('#calificacionNumero').style.display = 'inline';
+        // console.log(tipo)
+    } else {
+        document.querySelector('#calificacionNumero').style.display = 'none';
+        document.querySelector('#calificacionLetras').style.display = 'inline';
+        // console.log(tipo)
+    }
+}
+
 function validar(){
     if(nombre === '' & calificacion === '' & creditos === ''){
-        alert('Todos los campos deben estar llenos');
+        console.log('******Todos los campos deben estar llenos******');
     } else if(nombre === ''){
-        alert('El Campo de asignatura debe estar lleno');
+        console.log('******El Campo de asignatura debe estar lleno******');
     } else if(calificacion === ''){
-        alert('El Campo de calificaion debe estar lleno');
+        console.log('******El Campo de calificaion debe estar lleno******');
     } else if(creditos === ''){
-        alert('El Campo de creditos debe estar lleno');
+        console.log('******El Campo de creditos debe estar lleno******');
     } else {
         return true;
     }
 }
+    
 
 function agregar(){
-    tabla.innerHTML += '<tr> <td>' + nombre + ' </td>'
-    + '<td>' + calificacion + ' </td>'
-    + '<td>' + creditos + ' </td> </tr>';
+    let calificacionMostrar;
 
+    if(tipo === 'base100'){
+        calificacionInput = document.querySelector('#calificacionNumero');
+        calificacionMostrar = calificacion;
+    } else {
+        calificacionInput = document.querySelector('#calificacionLetras');
+        if(calificacion == 4){
+            calificacionMostrar = 'A';
+        } else if(calificacion == 3 ){
+            calificacionMostrar = 'B';
+        } else if(calificacion == 2){
+            calificacionMostrar = 'C';
+        } else if(calificacion == 1){
+            calificacionMostrar = 'D';
+        } else if(calificacion == 0 ){
+            calificacionMostrar = 'F';
+        } 
+    }
+    console.log(calificacion)
+
+
+    tabla.innerHTML += '<tr> <td>' + nombre + ' </td>'
+    + '<td>' + calificacionMostrar + ' </td>'
+    + '<td>' + creditos + ' </td> </tr>';
     datos.creditos += parseInt(creditos);
     datos.puntosCalidad = parseInt(calificacion) * parseInt(creditos);
     datos.sumaPtsCalidad += datos.puntosCalidad;
@@ -48,14 +97,26 @@ function calcular(){
 
 function limpiar(){
     form.asignatura.value = '';
-    form.calificacion.value = '';
+    document.querySelector('#calificacionNumero').value = '';
+    document.querySelector('#calificacionLetras').value = 4;
+    calificacion = '';
     form.creditos.value = '';
 }
 
 btnAgregar.onclick = (evt) => {
     evt.preventDefault();
- 
-    nombre = nombreAsignatura.value;
+    for(let i = 0; i < calificacionTipo.length; i++){
+        if(calificacionTipo[i].checked){
+            tipo = calificacionTipo[i].value
+        }
+    }
+    if(tipo === 'base100'){
+        calificacionInput = document.querySelector('#calificacionNumero');
+    } else {
+        calificacionInput = document.querySelector('#calificacionLetras');  
+    }
+
+    nombre = (nombreAsignatura.value).trim();
     calificacion = calificacionInput.value;
     creditos = creditosInput.value;
 
@@ -79,10 +140,8 @@ btnCalcular.onclick = () =>{
     calcular();
 }
 
-// !Opcion 1 Las letras sean identificadas como numeros
-// Poner input calificacion que acepte letras y hacer una validacion de que los nummeros sean
-// de 0 a 100; y que las letras sea identificadas (por medio de un if) como numeros (4, 3, 2, 1, 0)
-// - (A, B, C, D, F) respectivamente
+
+
 
 // !Opcion 2 Seleccionar por un input radio si se utilizara base 100 o letras
 // un if verificando cual input se selecciono y trabaja en base a el seleccionado
